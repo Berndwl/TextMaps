f09436011c5c8bcde527f803b2ae797c0c6c13fc
Berndwl <Wolf@chat.org>
2022-01-08 10:11:39 +0100

    -> refactor download_page.js -> use puppeteer instead of deprecated phantomjs

--- tools/download_page.js
+++ tools/download_page.js
@@ -1,11 +1,8 @@
-// ####################################################################################
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-// --- RENDERING PAGES --- //
-var RenderUrlsToFile, arrayOfUrls, system, fs;
-var xpaths = [];
-var MAX_WIDTH = 1280;
-system = require("system");
-fs = require('fs');
+const MAX_WIDTH = 1280;
+const MAX_HEIGHT = 1400;
 
 /*
 Render given urls
@@ -13,197 +10,188 @@ Render given urls
 @param output path
 */
 
-RenderUrl = function(url, output_path) {
-    var getImagePath, getImageFilename, getAnnotationPath, getListPath, formatNumberLength, next, page, retrieve, urlIndex, webpage;
-    
-    urlIndex = 0;
-    webpage = require("webpage");
-    page = null;
-    
-    saveDomTree = function(dom_tree_path, dom_tree) {    
-        var dom_content = JSON.stringify(dom_tree);
-        fs.write(dom_tree_path, dom_content, 'w');
-    };
-    
-    getImagePath = function(output_path) {
-        return output_path+"/screenshot.jpeg"
-    }
+const RenderUrl = (url, output_path) => {
 
-    getDOMPath = function(output_path){
-        return output_path+"/dom.json"
-    }
+	const saveDomTree = (dom_tree_path, dom_tree) => {
+		const dom_content = JSON.stringify(dom_tree);
+		fs.writeFile(dom_tree_path, dom_content, (err) => {
+			if(err) {
+				return console.log(err);
+			}
+		});
+	};
 
-    // returns DOM tree root with all its descendents
-    // each node includes additional useful information - such as position, etc. 
-    getDOMTree = function(){
-        baseurl = window.location
+	const getImagePath = (output_path) => {
+		return output_path + "/screenshot.png"
+	};
 
-        var selected_style_props = ['display','visibility','opacity','z-index','background-image','content','image'];
+	const getDOMPath = (output_path) => {
+		return output_path + "/dom.json"
+	};
 
-        //-- get elements in processing order
-        getElements = function(){
-            var tree_stack = new Array();
-            var result_stack = new Array();
-            tree_stack.push(document);
-            // if we have some other nodes
-            while (tree_stack.length != 0){
-                // get element
-                el = tree_stack.pop();
-                // put it in result stack
-                result_stack.push(el);
-                //add children of element to stack
-                for (i=0;i<el.childNodes.length;i++){
-                    tree_stack.push(el.childNodes[i])
-                }
-            }
-            return result_stack
-        }
+	// returns DOM tree root with all its descendents
+	// each node includes additional useful information - such as position, etc.
+	const getDOMTree = () => {
+		const selected_style_props = ['display', 'visibility', 'opacity', 'z-index', 'background-image', 'content', 'image'];
 
-        //-- creates node with all information
-        createNode = function(element){
-            node = {};
-            node.name = element.nodeName;
-            node.type = element.nodeType;
+		//-- get elements in processing order
+		const getElements = () => {
+			let tree_stack = [];
+			let result_stack = [];
 
-            //VALUE
-            if (element.nodeValue){
-                node.value =element.nodeValue;
-            }
-            //COMPUTED STYLE
-            computed_style = window.getComputedStyle(element);    
-            if (computed_style){
-                node.computed_style = {}
-                for (i=0;i<selected_style_props.length;i++){
-                    style_prop = selected_style_props[i]
-                    node.computed_style[style_prop]=computed_style[style_prop]
-                }
-            }
-            //POSITION
-            try{
-                // IT HAS BOUNDINGCLIENTRECT
-                if(typeof element.getBoundingClientRect === 'function') {
-                    bb = element.getBoundingClientRect()
-                    node.position = [Math.round(bb.left), Math.round(bb.top), Math.round(bb.right), Math.round(bb.bottom)]
-                // TRY TO COMPUTE IT
-                }else{
-                    bb = null
-                    var range = document.createRange();
-                    range.selectNodeContents(element);
-                    bb = range.getBoundingClientRect();
-                    if (bb){
-                        node.position = [Math.round(bb.left), Math.round(bb.top), Math.round(bb.right), Math.round(bb.bottom)]
-                    }
-                }
-            }catch(err){} 
-            // ATRIBTURES
-            attrs = element.attributes
-            if (attrs){
-                node.attrs = {}
-                for (i=0;i<attrs.length;i++){
-                    node.attrs[attrs[i].nodeName] = attrs[i].nodeValue
-                }
-            }
-            return node
-        }
+			tree_stack.push(document);
+			// if we have some other nodes
+			while (tree_stack.length !== 0) {
+				// get element
+				let element = tree_stack.pop();
+				// put it in result stack
+				result_stack.push(element);
+				//add children of element to stack
+				for (let i = 0; i < element.childNodes.length; i++) {
+					tree_stack.push(element.childNodes[i])
+				}
+			}
+			return result_stack
+		};
 
-        //---------- RUN -----------//
-        element_stack = getElements()
-        processed_stack = new Array();
+		//-- creates node with all information
+		const createNode = (element) => {
+			let node = {
+				name: element.name,
+				type: element.nodeType
+			};
 
-        while (element_stack.length != 0){
-            element = element_stack.pop();
-            // node
-            node = createNode(element)
-            // add children
-            if (element.childNodes.length>0){
-                node.childNodes = []
-                for (i=0; i<element.childNodes.length;i++){
-                    childNode = processed_stack.pop();
-                    node.childNodes.unshift(childNode);
-                }
-            }
-            // add result to stack
-            processed_stack.push(node)        
-            //console.log(processed_stack.length)
-        }
-        return processed_stack.pop()
-    }
+			//VALUE
+			if (element.nodeValue) {
+				node.value = element.nodeValue;
+			}
 
-            
-    page = webpage.create();
-    page.viewportSize = {
-        width: MAX_WIDTH,
-        height: 800
-    };
+			//COMPUTED STYLE
+			let computed_style = typeof(element) == "Element" ? window.getComputedStyle(element) : null;
+			if (computed_style) {
+				node.computed_style = {};
+				for (let i = 0; i < selected_style_props.length; i++) {
+					let style_prop = selected_style_props[i];
+					node.computed_style[style_prop] = computed_style[style_prop]
+				}
+			}
 
-    page.settings.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X) AppleWebKit/538.1 (KHTML, like Gecko) PhantomJS/2.0.0 Safari/538.1; CVUT-Cloud_Computing_Center BOT (http://3c.felk.cvut.cz/bot/)"
-    page.onConsoleMessage = function(msg) {
-      console.log(msg);
-    };
+			let boundingClientRect = null;
 
-    page.open(url, function(status) {
-        var file;
-       
-        image_path = getImagePath(output_path);
-        dom_tree_path = getDOMPath(output_path);
-        console.log(image_path)
+			//POSITION
+			try {
+				// IT HAS BOUNDINGCLIENTRECT
+				if (typeof element.getBoundingClientRect === 'function') {
+					boundingClientRect = element.getBoundingClientRect();
 
-        //succeded
-        if (status === "success") {
-            console.log('success')
+					node.position = [
+						Math.round(boundingClientRect.left),
+						Math.round(boundingClientRect.top),
+						Math.round(boundingClientRect.right),
+						Math.round(boundingClientRect.bottom)
+					]
+				}
+				// TRY TO COMPUTE IT
+				else {
+					const range = document.createRange();
+					range.selectNodeContents(element);
+					boundingClientRect = range.getBoundingClientRect();
 
-            // after 200 ms - press ESCAPE
-            window.setTimeout((function() {
-                page.sendEvent('keypress', page.event.key.Escape);
-                // page.sendEvent('click', 1199, 1, button='left');
-            }), 1000);
+					if (boundingClientRect) {
 
-            // after 2000 ms - start parsing and rendering
-            window.setTimeout((function() {
-                    var html = page.content
-                    var dom_tree = page.evaluate(getDOMTree);
+						node.position = [
+							Math.round(boundingClientRect.left),
+							Math.round(boundingClientRect.top),
+							Math.round(boundingClientRect.right),
+							Math.round(boundingClientRect.bottom)
+						]
+					}
+				}
+			}
+			catch (err) {}
 
-                    // before rendering
-                    // add white background in order to override black jpeg default
-                    page.evaluate(function() {
-                      var style = document.createElement('style'),
-                          text = document.createTextNode('body { background: #ffffff }');
-                      style.setAttribute('type', 'text/css');
-                      style.appendChild(text);
-                      document.head.insertBefore(style, document.head.firstChild);
-                    });
+			let attributes = element.attributes;
+			if (attributes) {
+				node.attrs = {};
+				for (let i = 0; i < attributes.length; i++) {
+					node.attrs[attributes[i].nodeName] = attributes[i].nodeValue
+				}
+			}
+			return node
+		};
 
-                    // save screenshot
-                    page.render(image_path,{format: 'jpeg', quality: '100'});
-                    
-                    // save DOM tree
-                    saveDomTree(dom_tree_path, dom_tree)
-                    page.close();
-                    phantom.exit();
-            }), 2000);
-            // phantom.exit();
-        } 
+		//---------- RUN -----------//
+		let element_stack = getElements();
+		let processed_stack = [];
 
-        // not succeded
-        else {
-            console.log('not success')
-            phantom.exit(1);
-        }
-    });
+		while (element_stack.length !== 0) {
+			let element = element_stack.pop();
+			let node = createNode(element);
+
+			// add children
+			if (element.childNodes.length > 0) {
+				node.childNodes = [];
+				for (let i = 0; i < element.childNodes.length; i++) {
+					let childNode = processed_stack.pop();
+					node.childNodes.unshift(childNode);
+				}
+			}
+			// add result to stack
+			processed_stack.push(node)
+			//console.log(processed_stack.length)
+		}
+		return processed_stack.pop()
+	};
+
+	const asyncTimeout = (ms) => {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	};
+
+	(async () => {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.setViewport({
+			width: MAX_WIDTH,
+			height: MAX_HEIGHT
+		});
+
+		let image_path = getImagePath(output_path);
+		let dom_tree_path = getDOMPath(output_path);
+
+		await page.goto(url);
+
+		await asyncTimeout(2000);
+
+		let dom_tree = await page.evaluate(getDOMTree);
+
+		// before rendering
+		// add white background in order to override black jpeg default
+		await page.evaluate(function () {
+			const style = document.createElement('style');
+			const text = document.createTextNode('body { background: #ffffff }');
+
+			style.setAttribute('type', 'text/css');
+			style.appendChild(text);
+
+			document.head.insertBefore(style, document.head.firstChild);
+		});
+
+		await page.screenshot({path: `${image_path}`});
+
+		// save DOM tree
+		saveDomTree(dom_tree_path, dom_tree);
+
+		await browser.close();
+	})();
 };
 
-// ################################################################################
-// ################################## MAIN PART ###################################
-// ################################################################################
-
 // --- READ PARAMS --- //
-if (system.args.length == 3) {
-    var url = system.args[1];
-    var output_path = system.args[2];
+if (process.argv.length === 4) {
+	let url = process.argv[2];
+	let output_path = process.argv[3];
+
+	RenderUrl(url, output_path);
 } else {
-    console.log("Usage: phantomjs download_page.js URL OUTPUT_PATH");
-    phantom.exit(1);
+	console.log("Usage: download_page.js URL OUTPUT_PATH");
 }
 
-// --- RUN --- //
-RenderUrl(url, output_path);
